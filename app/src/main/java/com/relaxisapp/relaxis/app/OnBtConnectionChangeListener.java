a48923aa8d0ac156c58d275be5de6d2f6a2023e6
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2014-04-29 21:49:43 +0300

    Trying to use the newer project

--- app/src/main/java/com/relaxisapp/relaxis/app/OnBtConnectionChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.relaxisapp.relaxis.app;
-
-import android.widget.Button;
-
-public interface OnBtConnectionChangeListener {
-	
-	public void onBtConnectionChange(int connectionState, Button button);
-	
-}
