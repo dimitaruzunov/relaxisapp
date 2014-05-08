c4dd17dac478a37368edd788077313f39518431a
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2014-05-08 19:07:40 +0300

    Added a % symbol after relax level number

--- app/src/main/java/com/relaxisapp/relaxis/views/StressView.java
+++ app/src/main/java/com/relaxisapp/relaxis/views/StressView.java
@@ -5,17 +5,12 @@ import android.content.Context;
 import android.graphics.Point;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Display;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.relaxisapp.relaxis.R;
-import com.relaxisapp.relaxis.activities.MainActivity;
-import com.relaxisapp.relaxis.activities.StressEstimationFragment;
 import com.relaxisapp.relaxis.events.Event;
 import com.relaxisapp.relaxis.events.EventListener;
 import com.relaxisapp.relaxis.models.StressModel;
@@ -56,9 +51,7 @@ public class StressView extends ScrollView {
     private void updateStressLevel() {
         int stressLevel = (int) (model.getStressLevel() * 100);
 
-        Log.d("Slash", String.valueOf((float) ((stressLevel * sliderWidth) / 100.0)));
-
-        stressLevelTextView.setText(String.valueOf(stressLevel));
+        stressLevelTextView.setText(String.valueOf(stressLevel) + "%");
         stressLevelTextView.setX((float) ((stressLevel * sliderWidth) / 100.0));
     }
 
